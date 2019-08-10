d405bf817c07c334d8676257b8cb5926ebde1e8f
yuki0920 <ywsep20@gmail.com>
2019-08-10 19:00:42 +0900

    管理画面とスーパーユーザー作成

--- todo/todo/models.py
+++ todo/todo/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    def __str__(self):
+        return self.title
