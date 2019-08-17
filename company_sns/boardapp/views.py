2550a8651025d1ca059bee344f26649724dd8577
yuki0920 <ywsep20@gmail.com>
2019-08-17 10:12:47 +0900

    登録データの重複制御 try/exept

--- company_sns/boardapp/views.py
+++ company_sns/boardapp/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 
 def signupfunc(request):
     if request.method == 'POST':
-        username = request.POST['username']
+        user_name = request.POST['username']
         password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
-        print(request.POST)
-        return render(request, 'signup.html', { 'some': 100 })
+        try:
+            User.objects.get(username=user_name)
+            return render(request, 'signup.html', { 'error': 'このユーザーは登録されています' })
+        except:
+            user = User.objects.create_user(user_name, '', password)
+            return render(request, 'signup.html', { 'some': 100 })
     return render(request, 'signup.html', { 'some' :100 })
