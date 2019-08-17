4356f02a4f03766ec1508ecd2bbaa3f2ce1c5027
yuki0920 <ywsep20@gmail.com>
2019-08-17 18:31:28 +0900

    listview

--- company_sns/boardapp/views.py
+++ company_sns/boardapp/views.py
@@ -25,3 +25,6 @@ def loginfunc(request):
         else:
             return redirect('login')
     return render(request, 'login.html')
+
+def listfunc(request):
+    return render(request, 'list.html')
