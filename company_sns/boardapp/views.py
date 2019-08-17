57ef95c968a76b209daa366ff262ce481d7a1a35
yuki0920 <ywsep20@gmail.com>
2019-08-17 09:01:14 +0900

    singupview

--- company_sns/boardapp/views.py
+++ company_sns/boardapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
+        print(request.POST)
+        return render(request, 'signup.html', { 'some': 100 })
     return render(request, 'signup.html', { 'some' :100 })
