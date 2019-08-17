6a365672b52f98b0c684a3444ed6145ce04c9107
yuki0920 <ywsep20@gmail.com>
2019-08-17 19:51:01 +0900

    imageファイル

--- company_sns/boardapp/models.py
+++ company_sns/boardapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    images = models.ImageField(upload_to='')
+    good = models.IntegerField()
+    read = models.IntegerField()
+    readtext = models.CharField(max_length=200)
--- company_sns/boardapp/urls.py
+++ company_sns/boardapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import signupfunc, loginfunc, listfunc
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
    path('signup/', signupfunc, name="signup"),
    path('login/', loginfunc, name="login"),
    path('list/', listfunc, name="list"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
